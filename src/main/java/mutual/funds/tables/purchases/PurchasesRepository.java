92498efc1d9ffbc27976f342705449ac43e2cc6d
Francesko Racaku <franceskoracaku@hotmail.com>
2022-02-24 16:40:00 -0500

    getting purchases by id

--- src/main/java/mutual/funds/tables/purchases/PurchasesRepository.java
+++ src/main/java/mutual/funds/tables/purchases/PurchasesRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PurchasesRepository
         extends JpaRepository<Purchases, Integer> {
+
+    List<Purchases> findAllById(Integer userId);
 
 //    @Query("SELECT s FROM Purchases s WHERE s.userId = ?1")
 //    Optional<Purchases> findPurchasesByUserId(String userId);
